dd09dae074b8884ef24a9cc13cc4429195798462
Asuncg <asun.c.garrido@gmail.com>
2024-03-18 14:47:59 +0100

    Primera subida de proyecto base

---
+++ src/main/java/es/asun/StoryCrafters/StoryCraftersApplication.java
@@ -0,0 +1,13 @@
+package es.asun.StoryCrafters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryCraftersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryCraftersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/asun/StoryCrafters/StoryCraftersApplicationTests.java
@@ -0,0 +1,13 @@
+package es.asun.StoryCrafters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryCraftersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
