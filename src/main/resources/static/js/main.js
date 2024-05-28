95d1cd8743fc88431a7e094ffcf0988ef94a102c
Asuncion Carmona Garrido <asun.c.garrido@gmail.com>
2024-05-28 20:29:20 +0200

    Se añade toggle ojo a restablecer password

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -17,3 +17,11 @@
   });
 
 })(jQuery);
+
+document.getElementById('togglePassword').addEventListener('click', function (e) {
+	const passwordInput = document.getElementById('newPassword');
+	const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+	passwordInput.setAttribute('type', type);
+	this.classList.toggle('bi-eye');
+	this.classList.toggle('bi-eye-slash');
+});
